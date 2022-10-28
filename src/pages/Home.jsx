ac3d6c46a014c8c8087d276054076577788745e1
MustafaEforte <mustafa@eforte.net>
2022-10-28 12:20:27 +0500

    ui-changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,67 @@
 import React from "react";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
+// import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import Drawer from "../components/Drawer";
-import { Button, Grid, TextField, Typography } from "@material-ui/core";
-import DeleteIcon from "@material-ui/icons/Delete";
-import CreateIcon from "@material-ui/icons/Create";
+// import { Button, Grid, TextField, Typography } from "@material-ui/core";
+import DeleteIcon from '@mui/icons-material/Delete';
+import CreateIcon from '@mui/icons-material/Create';
+// import EditIcon from '@mui/icons-material/Edit';
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, deleteUser } from "../redux/reducer";
 import BasicModal from "../components/Modal";
+import { Button, Typography, TextField, Grid } from "@mui/material";
+import { styled, useTheme, withStyles, makeStyles } from '@mui/material/styles';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 
-const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: "#3d85c6",
-    color: "white",
+// const StyledTableCell = styled((theme) => ({
+//   head: {
+//     backgroundColor: "#3d85c6",
+//     color: "white",
+//   },
+//   body: {
+//     fontSize: 14,
+//   },
+// }))(TableCell);
+
+// const StyledTableRow = styled((theme) => ({
+//   root: {
+//     "&:nth-of-type(odd)": {
+//       backgroundColor: theme.palette.action.hover,
+//     },
+//   },
+// }))(TableRow);
+
+// const useStyles = makeStyles({
+//   table: {
+//     marginTop: 30,
+//     minWidth: 700,
+//   },
+// });
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
   },
-  body: {
+  [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
   },
-}))(TableCell);
+}));
 
-const StyledTableRow = withStyles((theme) => ({
-  root: {
-    "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.action.hover,
-    },
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
   },
-}))(TableRow);
-
-const useStyles = makeStyles({
-  table: {
-    marginTop: 30,
-    minWidth: 700,
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
   },
-});
+}));
 
 export default function CustomizedTables() {
   const [id, setId] = useState("");
@@ -58,7 +80,7 @@ export default function CustomizedTables() {
   const [open, setOpen] = useState(false);
   const [addInput, setAddInput] = useState([]);
 
-  const classes = useStyles();
+  // const classes = useStyles();
 
   const handleAddClick = () => {
     setShowTextField(true);
@@ -112,7 +134,7 @@ export default function CustomizedTables() {
           </Grid>
         </Grid>
         <TableContainer component={Paper}>
-          <Table className={classes.table} aria-label="customized table">
+          <Table sx={{ minWidth: 700 }} aria-label="customized table">
             <TableHead>
               <TableRow>
                 <StyledTableCell>ID</StyledTableCell>
@@ -156,7 +178,8 @@ export default function CustomizedTables() {
                       Edit
                     </Button>
                     <Button
-                      color="secondary"
+                    sx={{ backgroundColor: "#ab003c" }}
+                      // color="secondary"
                       variant="contained"
                       size="small"
                       onClick={() => handleDelete(row.id)}
@@ -171,7 +194,7 @@ export default function CustomizedTables() {
           </Table>
         </TableContainer>
         <TableContainer component={Paper}>
-          <Table className={classes.table} aria-label="customized table">
+          <Table sx={{ minWidth: 700 }} aria-label="customized table">
             <TableBody>
               {showTextField === true
                 ? addInput.map((item, index) => (
